ce08c028bfff2afe3e6f164b0879964d095cb969
NazarLyubchinsky <nazar.lyubchinskyi@gmail.com>
2023-08-14 20:39:59 +0300

    fix localStr, adaptive, newMain

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,117 +1,132 @@
-// import React from "react";
+
+// import React, { useState, useEffect } from "react";
 // import io from "socket.io-client";
-// import { useEffect } from "react";
 // import { useLocation, useNavigate } from "react-router-dom";
-// import { useState } from "react";
 // import EmojiPicker from "emoji-picker-react";
 
 // import icon from "../images/emoji.svg";
-// import s from "../styles/Chat.module.css";
+// import s from "../styles/Chat.module.scss";
 // import Messages from "./Messages";
 
-// const socket = io.connect("https://onlinechat-zttn.onrender.com");
-
 // const Chat = () => {
-//   const { search } = useLocation();
-//   const navigate = useNavigate();
-//   const [params, setParams] = useState({ room: "", user: "" });
-//   const [state, setState] = useState([]);
-//   const [message, setMessage] = useState("");
-//   const [isOpen, setOpen] = useState(false);
-//   const [users, setUsers] = useState(0);
+// 	const { search } = useLocation();
+// 	const navigate = useNavigate();
+// 	const [params, setParams] = useState({ room: "", user: "" });
+// 	const [state, setState] = useState([]);
+// 	const [message, setMessage] = useState("");
+// 	const [isOpen, setOpen] = useState(false);
+// 	const [users, setUsers] = useState(0);
 
-//   useEffect(() => {
-//     const searchParams = Object.fromEntries(new URLSearchParams(search));
-//     setParams(searchParams);
-//     socket.emit("join", searchParams);
-//   }, [search]);
+// 	const socket = io.connect("https://onlinechat-zttn.onrender.com");
 
-//   useEffect(() => {
-//     socket.on("message", ({ data }) => {
-//       setState((_state) => [..._state, data]);
-//     });
-//   }, []);
+// 	useEffect(() => {
+// 		const searchParams = Object.fromEntries(new URLSearchParams(search));
+// 		setParams(searchParams);
+// 		socket.emit("join", searchParams);
 
-//   useEffect(() => {
-//     socket.on("room", ({ data: { users } }) => {
-//       setUsers(users.length);
-//     });
-//   }, []);
+// 		const handleMessages = ({ data }) => {
+// 			setState(prevState => [...prevState, data]);
+// 		};
 
-//   const leftRoom = () => {
-//     socket.emit("leftRoom", { params });
-//     navigate("/");
-//   };
+// 		const handleRoomInfo = ({ data: { users } }) => {
+// 			setUsers(users.length);
+// 		};
 
-//   const handleChange = ({ target: { value } }) => setMessage(value);
+// 		socket.on("message", handleMessages);
+// 		socket.on("room", handleRoomInfo);
 
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
+// 		return () => {
+// 			socket.off("message", handleMessages);
+// 			socket.off("room", handleRoomInfo);
+// 		};
+// 		// eslint-disable-next-line react-hooks/exhaustive-deps
+// 	}, [search]);
 
-//     if (!message) return;
+// 	useEffect(() => {
+// 		const savedMessages = localStorage.getItem(`chatMessages_${params.room}`);
+// 		setState(savedMessages ? JSON.parse(savedMessages) : []);
+// 	}, [params.room]);
 
-//     socket.emit("sendMessage", { message, params });
+// 	useEffect(() => {
+// 		localStorage.setItem(`chatMessages_${params.room}`, JSON.stringify(state));
+// 	}, [state, params.room]);
 
-//     setMessage("");
-//   };
+// 	const leaveRoom = () => {
+// 		socket.emit("leftRoom", { params });
+// 		navigate("/");
+// 	};
 
-//   const onEmojiClick = ({ emoji }) => setMessage(`${message} ${emoji}`);
+// 	const handleChange = (e) => setMessage(e.target.value);
 
-//   return (
-//     <div className={s.wrap}>
-//       <div className={s.header}>
-//         <div className={s.title}>{params.room}</div>
-//         <div className={s.users}>{users} users in this room</div>
-//         <button className={s.left} onClick={leftRoom}>
-//           Left the room
-//         </button>
-//       </div>
+// 	const handleSubmit = (e) => {
+// 		e.preventDefault();
 
-//       <div className={s.messages}>
-//         <Messages messages={state} name={params.name} />
-//       </div>
+// 		if (!message.trim()) return;
 
-//       <form className={s.form} onSubmit={handleSubmit}>
-//         <div className={s.input}>
-//           <input
-//             type="text"
-//             name="message"
-//             placeholder="What do you want to say?"
-//             value={message}
-//             onChange={handleChange}
-//             autoComplete="off"
-//             required
-//           />
-//         </div>
-//         <div className={s.emoji}>
-//           <img src={icon} alt="" onClick={() => setOpen(!isOpen)} />
+// 		socket.emit("sendMessage", { message, params });
+// 		setMessage("");
+// 	};
 
-//           {isOpen && (
-//             <div className={s.emojies}>
-//               <EmojiPicker onEmojiClick={onEmojiClick} />
-//             </div>
-//           )}
-//         </div>
+// 	const onEmojiClick = ({ emoji }) => setMessage(prevMessage => prevMessage + " " + emoji);
 
-//         <div className={s.button}>
-//           <input type="submit" onSubmit={handleSubmit} value="Send a message" />
-//         </div>
-//       </form>
-//     </div>
-//   );
+
+
+// 	return (
+// 		<section className={s.chat}>
+// 			<div className={s.wrap}>
+// 				<div className={s.header}>
+// 					<div className={s.title}>{params.room}</div>
+// 					<div className={s.users}>{users} users in this room</div>
+// 					<button className={s.left} onClick={leaveRoom}>
+// 						Leave the room
+// 					</button>
+// 				</div>
+
+// 				<div className={`${s.messages} ${s.scrollable}`}>
+// 					<Messages messages={state} name={params.name} />
+// 				</div>
+
+// 				<form className={s.form} onSubmit={handleSubmit}>
+// 					<div className={s.input}>
+// 						<input
+// 							type="text"
+// 							name="message"
+// 							placeholder="What do you want to say?"
+// 							value={message}
+// 							onChange={handleChange}
+// 							autoComplete="off"
+// 							required
+// 						/>
+// 					</div>
+// 					<div className={s.emoji}>
+// 						<img src={icon} alt="Emoji" onClick={() => setOpen(!isOpen)} />
+// 						{isOpen && (
+// 							<div className={s.emojies}>
+// 								<EmojiPicker onEmojiClick={onEmojiClick} />
+// 							</div>
+// 						)}
+// 					</div>
+// 					<div className={s.button}>
+// 						<button type="submit">Send a message</button>
+// 					</div>
+// 				</form>
+// 			</div>
+// 		</section>
+// 	);
 // };
 
 // export default Chat;
 
 
 
-import React, { useState, useEffect } from "react";
+
+import React, { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 import { useLocation, useNavigate } from "react-router-dom";
 import EmojiPicker from "emoji-picker-react";
 
 import icon from "../images/emoji.svg";
-import s from "../styles/Chat.module.css";
+import s from "../styles/Chat.module.scss";
 import Messages from "./Messages";
 
 const Chat = () => {
@@ -124,6 +139,14 @@ const Chat = () => {
 	const [users, setUsers] = useState(0);
 
 	const socket = io.connect("https://onlinechat-zttn.onrender.com");
+
+	// Scroll
+	const messagesRef = useRef(null);
+	useEffect(() => {
+		if (messagesRef.current) {
+			messagesRef.current.scrollTop = messagesRef.current.scrollHeight; 
+		}
+	}, [state]);
 
 	useEffect(() => {
 		const searchParams = Object.fromEntries(new URLSearchParams(search));
@@ -145,7 +168,7 @@ const Chat = () => {
 			socket.off("message", handleMessages);
 			socket.off("room", handleRoomInfo);
 		};
-	// eslint-disable-next-line react-hooks/exhaustive-deps
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [search]);
 
 	useEffect(() => {
@@ -175,46 +198,53 @@ const Chat = () => {
 
 	const onEmojiClick = ({ emoji }) => setMessage(prevMessage => prevMessage + " " + emoji);
 
+
+
 	return (
-		<div className={s.wrap}>
-			<div className={s.header}>
-				<div className={s.title}>{params.room}</div>
-				<div className={s.users}>{users} users in this room</div>
-				<button className={s.left} onClick={leaveRoom}>
-					Leave the room
-				</button>
-			</div>
+		<section className={s.chat}>
+			<div className={s.wrap}>
+				<div className={s.header}>
+					<div className={s.title}>{params.room}</div>
+					<div className={s.users}>{users} users in this room</div>
+					<button className={s.left} onClick={leaveRoom}>
+						Leave the room
+					</button>
+				</div>
 
-			<div className={s.messages}>
-				<Messages messages={state} name={params.name} />
-			</div>
+				<div className={`${s.messages} ${s.scrollable}`} ref={messagesRef}>
+					<Messages messages={state} name={params.name} />
+				</div>
 
-			<form className={s.form} onSubmit={handleSubmit}>
-				<div className={s.input}>
-					<input
-						type="text"
-						name="message"
-						placeholder="What do you want to say?"
-						value={message}
-						onChange={handleChange}
-						autoComplete="off"
-						required
-					/>
-				</div>
-				<div className={s.emoji}>
-					<img src={icon} alt="Emoji" onClick={() => setOpen(!isOpen)} />
-					{isOpen && (
-						<div className={s.emojies}>
-							<EmojiPicker onEmojiClick={onEmojiClick} />
-						</div>
-					)}
-				</div>
-				<div className={s.button}>
-					<button type="submit">Send a message</button>
-				</div>
-			</form>
-		</div>
+				<form className={s.form} onSubmit={handleSubmit}>
+					<div className={s.input}>
+						<input
+							type="text"
+							name="message"
+							placeholder="What do you want to say?"
+							value={message}
+							onChange={handleChange}
+							autoComplete="off"
+							required
+						/>
+					</div>
+					<div className={s.emoji}>
+						<img src={icon} alt="Emoji" onClick={() => setOpen(!isOpen)} />
+						{isOpen && (
+							<div className={s.emojies}>
+								<EmojiPicker onEmojiClick={onEmojiClick} />
+							</div>
+						)}
+					</div>
+					<div className={s.button}>
+						<button type="submit">Send a message</button>
+					</div>
+				</form>
+			</div>
+		</section>
 	);
 };
 
 export default Chat;
+
+
+
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,72 +2,105 @@ import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-import s from "../styles/Main.module.css";
+import s from "../styles/Main.module.scss";
+
+// images
+import GITHUB from '../images/git.svg'
+import FACEBOOK from '../images/face.svg'
+import INSTAGRAM from '../images/inst.svg'
 
 const FIELDS = {
-  NAME: "name",
-  ROOM: "room",
+	NAME: "name",
+	ROOM: "room",
 };
 
 const Main = () => {
-  const { NAME, ROOM } = FIELDS;
+	const { NAME, ROOM } = FIELDS;
 
-  const [values, setValues] = useState({ [NAME]: "", [ROOM]: "" });
+	const [values, setValues] = useState({ [NAME]: "", [ROOM]: "" });
 
-  const handleChange = ({ target: { value, name } }) => {
-    setValues({ ...values, [name]: value });
-  };
+	const handleChange = ({ target: { value, name } }) => {
+		setValues({ ...values, [name]: value });
+	};
 
-  const handleClick = (e) => {
-    const isDisabled = Object.values(values).some((v) => !v);
+	const handleClick = (e) => {
+		const isDisabled = Object.values(values).some((v) => !v);
 
-    if (isDisabled) e.preventDefault();
-  };
+		if (isDisabled) e.preventDefault();
+	};
 
-  return (
-    <div className={s.wrap}>
-      <div className={s.container}>
-        <h1 className={s.heading}>Join</h1>
+	return (
+		<div>
+			<header className={s.header}>
+				<nav className={s.nav}>
+					<Link to='/'>home</Link>
+					<Link to='/'>contact</Link>
+				</nav>
+				<div>
+					<i className={s.fa_solid}>lor</i>
+				</div>
+			</header>
+			<div className={s.background}></div>
+			<section className={s.home}>
+				<div className={s.content}>
+					<a href="/" className={s.logo}>Chat</a>
+					<h2>Welcom</h2>
+					<h3>to our new chat</h3>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, molestias!</p>
+					<div className={s.icon}>
+						<a href='https://instagram.com' target='_blank' rel='noopener noreferrer' className={s.fa_brands}>
+							<img src={INSTAGRAM} alt='Instagram' />
+						</a>
+						<a href='https://facebook.com' target='_blank' rel='noopener noreferrer' className={s.fa_brands}>
+							<img src={FACEBOOK} alt={FACEBOOK} />
+						</a>
+						<a href='https://github.com/NazarLyubchinsky' target='_blank' rel='noopener noreferrer' className={s.fa_brands}>
+							<img src={GITHUB} alt={GITHUB} />
+						</a>
+					</div>
+				</div>
+				<div className={s.login}>
+					<h1 className={s.heading}>Join</h1>
+					<form className={s.form}>
+						<div className={s.group}>
+							<input
+								type="text"
+								name="name"
+								value={values[NAME]}
+								placeholder="Username"
+								className={s.input}
+								onChange={handleChange}
+								autoComplete="off"
+								required
+							/>
+						</div>
+						<div className={s.group}>
+							<input
+								type="text"
+								name="room"
+								placeholder="Room"
+								value={values[ROOM]}
+								className={s.input}
+								onChange={handleChange}
+								autoComplete="off"
+								required
+							/>
+						</div>
 
-        <form className={s.form}>
-          <div className={s.group}>
-            <input
-              type="text"
-              name="name"
-              value={values[NAME]}
-              placeholder="Username"
-              className={s.input}
-              onChange={handleChange}
-              autoComplete="off"
-              required
-            />
-          </div>
-          <div className={s.group}>
-            <input
-              type="text"
-              name="room"
-              placeholder="Room"
-              value={values[ROOM]}
-              className={s.input}
-              onChange={handleChange}
-              autoComplete="off"
-              required
-            />
-          </div>
-
-          <Link
-            className={s.group}
-            onClick={handleClick}
-            to={`/chat?name=${values[NAME]}&room=${values[ROOM]}`}
-          >
-            <button type="submit" className={s.button}>
-              Sign In
-            </button>
-          </Link>
-        </form>
-      </div>
-    </div>
-  );
+						<Link
+							className={s.group}
+							onClick={handleClick}
+							to={`/chat?name=${values[NAME]}&room=${values[ROOM]}`}
+						>
+							<button type="submit" className={s.button}>
+								Sign In
+							</button>
+						</Link>
+					</form>
+				</div>
+			</section >
+		</div >
+	);
 };
 
 export default Main;

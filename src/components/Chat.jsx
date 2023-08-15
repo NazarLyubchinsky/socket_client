efe178e4fd33b65dde3664c712145c94ea23e254
NazarLyubchinsky <nazar.lyubchinskyi@gmail.com>
2023-08-15 17:41:16 +0300

    fix adaptive

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -125,6 +125,7 @@ import io from "socket.io-client";
 import { useLocation, useNavigate } from "react-router-dom";
 import EmojiPicker from "emoji-picker-react";
 
+
 import icon from "../images/emoji.svg";
 import s from "../styles/Chat.module.scss";
 import Messages from "./Messages";
@@ -137,6 +138,7 @@ const Chat = () => {
 	const [message, setMessage] = useState("");
 	const [isOpen, setOpen] = useState(false);
 	const [users, setUsers] = useState(0);
+	const [hasContent, setHasContent] = useState(false);
 
 	const socket = io.connect("https://onlinechat-zttn.onrender.com");
 
@@ -144,7 +146,7 @@ const Chat = () => {
 	const messagesRef = useRef(null);
 	useEffect(() => {
 		if (messagesRef.current) {
-			messagesRef.current.scrollTop = messagesRef.current.scrollHeight; 
+			messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
 		}
 	}, [state]);
 
@@ -185,7 +187,10 @@ const Chat = () => {
 		navigate("/");
 	};
 
-	const handleChange = (e) => setMessage(e.target.value);
+	const handleChange = (e) => {
+		setMessage(e.target.value);
+		setHasContent(e.target.value.trim() !== '');
+	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -198,17 +203,43 @@ const Chat = () => {
 
 	const onEmojiClick = ({ emoji }) => setMessage(prevMessage => prevMessage + " " + emoji);
 
+	const [width, setWidth] = useState(400);
+	const [height, setHeight] = useState(400);
+
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth <= 450) {
+				setWidth(280);
+				setHeight(340);
+			} else {
+				setWidth(350);
+			}
+		};
+
+		handleResize(); // Визначити початкове значення ширини
+
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
 
 
 	return (
 		<section className={s.chat}>
 			<div className={s.wrap}>
 				<div className={s.header}>
-					<div className={s.title}>{params.room}</div>
-					<div className={s.users}>{users} users in this room</div>
-					<button className={s.left} onClick={leaveRoom}>
-						Leave the room
-					</button>
+					<div className={s.header__top}>
+						<div className={s.title}>{params.room}</div>
+						<div className={s.users}>{users} users in this room</div>
+					</div>
+					<div className={s.header__bottom}>
+						<button className={s.left} onClick={leaveRoom}>
+							Leave the room
+						</button>
+
+					</div>
 				</div>
 
 				<div className={`${s.messages} ${s.scrollable}`} ref={messagesRef}>
@@ -216,6 +247,14 @@ const Chat = () => {
 				</div>
 
 				<form className={s.form} onSubmit={handleSubmit}>
+					<div className={s.emoji}>
+						<img src={icon} alt="Emoji" onClick={() => setOpen(!isOpen)} />
+						{isOpen && (
+							<div className={s.emojies}>
+								<EmojiPicker onEmojiClick={onEmojiClick} width={width} height={height} />
+							</div>
+						)}
+					</div>
 					<div className={s.input}>
 						<input
 							type="text"
@@ -227,16 +266,9 @@ const Chat = () => {
 							required
 						/>
 					</div>
-					<div className={s.emoji}>
-						<img src={icon} alt="Emoji" onClick={() => setOpen(!isOpen)} />
-						{isOpen && (
-							<div className={s.emojies}>
-								<EmojiPicker onEmojiClick={onEmojiClick} />
-							</div>
-						)}
-					</div>
+
 					<div className={s.button}>
-						<button type="submit">Send a message</button>
+						<button type="submit" className={hasContent ? s.activeButton : s.buttonSub}>Send</button>
 					</div>
 				</form>
 			</div>
